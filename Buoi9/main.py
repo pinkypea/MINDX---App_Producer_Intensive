7dd536b01b5f5bfa18ceed3c96e87f0163c2b6a8
KienDao <daokien2303@gmail.com>
2025-04-12 09:56:46 +0700

    Buổi 9: Thêm chức năng Thêm phim mới

---
+++ Buoi9/main.py
@@ -0,0 +1,89 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication, QMessageBox, QDialog
+import sys
+from PyQt6 import uic
+from PyQt6.QtCore import Qt
+from models import MoviesList, Movies
+
+from PyQt6.QtCore import QDate
+from datetime import datetime
+
+class AddDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi("Buoi9/add_dialog.ui", self)
+
+        # Format lại ngày tháng năm
+        self.ui.dateInput.setDisplayFormat("dd/MM/yyyy")
+
+        # Xử lý 2 nút OK và Cancel
+        self.ui.buttonBox.accepted.connect(self.accept)
+        self.ui.buttonBox.rejected.connect(self.reject)
+
+    def return_input_fields(self) -> dict:
+        date_input = self.ui.dateInput.date().toPyDate()
+
+        # Trả dữ liệu về
+        return {
+            "id": self.ui.idInput.text(),
+            "title": self.ui.titleInput.text(),
+            "release_date": date_input.strftime("%b %Y"),
+            "rating": float(self.ui.ratingInput.text() or 0.0),
+            "link": self.ui.linkInput.text()
+        }
+
+class CRUD_Page(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi("Buoi9/CRUD.ui", self)
+
+        self.database = MoviesList()
+        self.database.load_from_json()
+
+        self.create_CRUD_page()
+
+        self.ui.removeButton.clicked.connect(self.delete)
+        self.ui.addButton.clicked.connect(self.add)
+
+    def create_CRUD_page(self):
+        movie_title = self.database.get_movie_title_list()
+        self.ui.movieList.addItems(movie_title)
+        self.ui.movieList.setCurrentRow(0)
+
+    def add(self):
+        current_index = self.ui.movieList.currentRow()
+        add_dialog = AddDialog()
+        # Nếu nhấn nút OK
+        if add_dialog.exec():
+            # Lấy dữ liệu từ dialog
+            inputs = add_dialog.return_input_fields()
+            movie = Movies(**inputs)
+            # Thêm dữ liệu vào list widget
+            self.ui.movieList.insertItem(current_index, inputs["title"])
+            # Thêm dữ liệu vào json
+            self.database.add_movie(movie)
+
+    def delete(self):
+        # Lấy items hiện tại
+        current_index = self.ui.movieList.currentRow()
+        item = self.ui.movieList.item(current_index)
+        item_title = item.text()
+
+        # Tạo message box
+        if item is not None:
+            choice = QMessageBox.question(self, "Remove movie",
+                                            "Do you want to remove this movie?",
+                                            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            
+            # Nếu người dùng ấn "Yes" thì xoá phim
+            if choice == QMessageBox.StandardButton.Yes:
+                item = self.ui.movieList.takeItem(current_index)
+                # Xoá dữ liệu trong DB
+                self.database.remove_movie(item_title)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    CRUDpage = CRUD_Page()
+    CRUDpage.show()
+    app.exec()
---
+++ Buoi9/models.py
@@ -0,0 +1,148 @@
+import json
+
+# Class Movies để khởi tạo các bộ phim
+class Movies:
+    def __init__(self, id, title, release_date, rating = None, link = None):
+        self.id = id
+        self.title = title
+        self.release_date = release_date
+        self.rating = float(rating) if rating else 0
+        self.link = link
+
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "title" : self.title,
+            "release_date" : self.release_date,
+            "rating" : self.rating,
+            "link" : self.link
+        }
+
+# Class MoviesList để quản lý danh sách phim
+class MoviesList:
+    def __init__(self):
+        self.movie_list = []
+
+    # Thêm phim mới
+    def add_movie(self, movie):
+        self.movie_list.append(movie)
+        self.save_to_json()
+
+    # Xoá phim
+    def remove_movie(self, identifier):
+        for movie in self.movie_list:
+            if movie.title == identifier or movie.id == identifier:
+                self.movie_list.remove(movie)
+        self.save_to_json()
+
+    # Cập nhật phim
+    def update_movie(self, movie_id, new_id, new_title, new_release_date, new_rating = None, new_link = None):
+        for movie in self.movie_list:
+            if movie.id == movie_id:
+                movie.update(new_id, new_title, new_release_date, new_rating, new_link)
+                return True
+        self.save_to_json()
+        return False
+
+    # Tìm kiếm phim bằng tên
+    def search_movie(self, title):
+        return [movie for movie in self.movie_list if title.lower() in movie.title.lower()]
+
+    # Trả về tên phim viết in thường
+    def get_movie_title(self, movie):
+        return movie.title.lower()
+    
+    # Trả về điểm đánh giá của phim
+    def get_movie_rating(self, movie):
+        return movie.rating
+    
+    # Trả về ngày tháng năm phát hành phim
+    def get_movie_release_date(self, movie):
+        # Chuyển ngày tháng năm từ string thành dạng danh sách
+        day, month, year = map(int, movie.release_date.split("/"))
+        return (year, month, day)
+
+    # Sắp xếp phim
+    def sort_movie(self, key="title"):
+        if key == "title":
+            self.movie_list.sort(key = self.get_movie_title)
+
+        elif key == "rating":
+            self.movie_list.sort(key = self.get_movie_rating)
+
+        elif key == "release_date":
+            self.movie_list.sort(key = self.get_movie_release_date)
+
+    # In ra danh sách phim
+    def display_movie(self):
+        for movie in self.movie_list:
+            print(f"{movie.id} - {movie.title} - {movie.release_date} - {movie.rating} - {movie.link}")
+
+    def save_to_json(self):
+        with open("./Buoi9/data.json", "w", encoding="utf-8") as file:
+            json.dump([movie.to_dict() for movie in self.movie_list], file, indent=4 ,ensure_ascii=False)
+
+    def load_from_json(self):
+        with open("./Buoi9/data.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+            self.movie_list = [self.create_movie_from_dict(movie) for movie in data]
+
+    def create_movie_from_dict(self, data):
+        return Movies(data["id"], data["title"], data["release_date"], data.get("rating"), data.get("link"))
+    
+    def get_movie_title_list(self):
+        return [movie.title for movie in self.movie_list]
+
+movie_list = MoviesList()
+
+# Khởi tạo các đối tượng từ class Movies
+movie1 = Movies(1, "Avengers: End game", "26/04/2019", 8.4)
+movie2 = Movies(2, "Terrifier 3", "19/09/2024", 6.3)
+movie3 = Movies(3, "Kamen Rider Decade", "30/08/2009")
+movie4 = Movies(4, "Phinies and Ferb", "17/08/2007")
+
+print("\n-----------------")
+print("DANH SÁCH PHIM BAN ĐẦU")
+movie_list.display_movie()
+
+# Kết quả khi thêm phim mới
+print("\n-----------------")
+print("DANH SÁCH PHIM KHI THÊM PHIM MỚI")
+movie_list.add_movie(movie1)
+movie_list.add_movie(movie2)
+movie_list.add_movie(movie3)
+movie_list.add_movie(movie4)
+movie_list.display_movie()
+# movie_list.save_to_json()
+
+# # Kết quả khi xoá phim
+# print("\n-----------------")
+# print("DANH SÁCH PHIM KHI XOÁ PHIM")
+# movie_list.remove_movie("Terrifier 3")
+# movie_list.display_movie()
+# movie_list.save_to_json()
+
+# # Kết quả khi tìm kiếm phim
+# result_movie = movie_list.search_movie("kamen")
+# print("\n-----------------")
+# print("DANH SÁCH PHIM KHI TÌM KIẾM PHIM")
+# for movie in result_movie:
+#     print(movie.title)
+
+# # Sắp xếp phim theo tên
+# movie_list.sort_movie(key="title")
+# print("\n-----------------")
+# print("SẮP XẾP PHIM THEO TÊN")
+# movie_list.display_movie()
+
+# # Sắp xếp phim theo rating
+# movie_list.sort_movie(key="rating")
+# print("\n-----------------")
+# print("SẮP XẾP PHIM THEO RATING")
+# movie_list.display_movie()
+
+# # Sắp xếp phim theo ngày phát hành
+# movie_list.sort_movie(key="release_date")
+# print("\n-----------------")
+# print("SẮP XẾP PHIM THEO NGÀY PHÁT HÀNH")
+# movie_list.display_movie()
