4c6463e4a1994436b8c2c6ad314e719e1b9b2037
KienDao <daokien2303@gmail.com>
2025-04-16 21:00:48 +0700

    thêm tính năng thêm item

--- Lesson_8/main.py
+++ Lesson_8/main.py
@@ -1,7 +1,34 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QDialog
 from PyQt6 import uic
 import sys
 from models import MovieList
+from models import Movies
+
+from PyQt6.QtCore import QDate
+from datetime import datetime
+
+class AddDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi("Lesson_8/add-dialog.ui", self)
+
+        # Format lại ngày tháng năm (Long và Vinh không cần ghi)
+        self.ui.dateInput.setDisplayFormat("dd/MM/yyyy")
+
+        # Xử lý nút OK/Cancel
+        self.ui.buttonBox.accepted.connect(self.accept)
+        self.ui.buttonBox.rejected.connect(self.reject)
+
+    def return_input_fields(self) -> dict:
+        date_input = self.ui.dateInput.date().toPyDate() #Long và Vinh không cần ghi
+
+        return {
+            "id": self.ui.idInput.text(),
+            "title": self.ui.titleInput.text(),
+            "release_date": date_input.strftime("dd/MM/yyyy"),
+            "rating": float(self.ui.ratingInput.text() or 0.0),
+            "link": self.ui.linkInput.text()
+        }
 
 class ListWidget(QMainWindow):
     def __init__(self):
@@ -13,10 +40,27 @@ class ListWidget(QMainWindow):
 
         self.setup_CRUD_page()
 
+        self.deleteButton.clicked.connect(self.delete)
+        self.addButton.clicked.connect(self.add)
+
     def setup_CRUD_page(self):
         movie_title = self.database.get_title_list()
         self.ui.movieList.addItems(movie_title)
         self.ui.movieList.setCurrentRow(0)
+
+    def add(self):
+        current_index = self.ui.movieList.currentRow()
+        
+        # Khởi tạo add dialog
+        add_dialog = AddDialog()
+        # Nếu ấn nút OK
+        if add_dialog.exec():
+            # Lấy dữ liệu từ dialog
+            inputs = add_dialog.return_input_fields()
+            movie = Movies(**inputs)
+            # Thêm item vào list widget
+            self.ui.movieList.insertItem(current_index, inputs["title"])
+            self.database.add_movie(movie)
 
     def delete(self):
         current_index = self.ui.movieList.currentRow()
@@ -30,7 +74,7 @@ class ListWidget(QMainWindow):
                                                 QMessageBox.StandardButton.Yes | 
                                                 QMessageBox.StandardButton.No)
             if choice == QMessageBox.StandardButton.Yes:
-                item = self.ui.movieList.takeItem(item_title)
+                item = self.ui.movieList.takeItem(current_index)
                 self.database.remove_movie(item_title)
 
 if __name__ == "__main__":
